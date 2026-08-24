4af3ddc306d2228f4499ffde97dfaf89d57bb645
Ariane-Kunda <arianekunda3@gmail.com>
2026-08-24 00:40:22 -1200

    App.jsx

--- frontend-app/src/App.jsx
+++ frontend-app/src/App.jsx
@@ -1,1 +1,65 @@
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+    Navigate
+} from "react-router-dom";
 
+import NavBar from "./components/NavBar";
+
+import Jobs from "./pages/Jobs";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import JobDetails from "./pages/JobDetails";
+
+export default function App() {
+
+    return (
+        <BrowserRouter>
+
+            <NavBar />
+
+            <main className="container">
+
+                <Routes>
+
+                    <Route
+                        path="/"
+                        element={
+                            <Navigate
+                                to="/jobs"
+                            />
+                        }
+                    />
+
+                    <Route
+                        path="/jobs"
+                        element={<Jobs />}
+                    />
+
+                    <Route
+                        path="/jobs/:id"
+                        element={
+                            <JobDetails />
+                        }
+                    />
+
+                    <Route
+                        path="/login"
+                        element={<Login />}
+                    />
+
+                    <Route
+                        path="/register"
+                        element={
+                            <Register />
+                        }
+                    />
+
+                </Routes>
+
+            </main>
+
+        </BrowserRouter>
+    );
+}
